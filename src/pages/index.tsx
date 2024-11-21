eacbf90910a14087dce1f42057f5d0b9435e7213
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-11-21 20:06:12 +0900

    feat: router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,22 @@ import {
 } from 'react-router-dom'
 import Signup from './Signup/Signup'
 import Home from './home'
+import Friend from './Friend/Friend'
+import Login from './Login/Login'
+import Chat from './Chat/Chat'
+import Profile from './Profile/Profile'
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<Home />}>
-        <Route path='signup' element={<Signup />} />
-        <Route path='login' />
-      </Route>,
+      <>
+        <Route index element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/friend' element={<Friend />} />
+        <Route path='/profile' element={<Profile />} />
+      </>,
     ),
   )
   return <RouterProvider router={router} />
