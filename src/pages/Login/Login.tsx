4daaf89092ad2c73542103cda1139554ff480c2c
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-01-10 17:30:25 +0900

    feat: Splash Component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react'
+import Splash from './Splash'
+import KUchat_Logo from '../../assets/images/KUchat_logo.svg'
+import GoogleImg from '../../assets/images/Login_Google_img.svg'
 
 const Login = () => {
-  return <div>Login</div>
+  const [showSplash, setShowSplash] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 300000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  const handleClick = () => {
+    window.location.href = 'https://www.kuchat.site/oauth2/authorization/google'
+  }
+
+  return showSplash ? (
+    <Splash />
+  ) : (
+    <div>
+      <div>
+        <img src={KUchat_Logo} alt='KU chat logo' />
+        <div>KU chat</div>
+        <button onClick={handleClick}>
+          <img src={GoogleImg} alt='Google logo' />
+          <div>Start with Google</div>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Login
---
+++ src/pages/Login/Splash.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import KUchat_Logo from '../../assets/images/KUchat_logo.svg'
+
+const Splash = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        padding-top: 30vh;
+        box-sizing: border-box;
+        align-items: center;
+        background-color: #046b40;
+        width: 100vw;
+        height: 100vh;
+        font-weight: 600;
+        font-size: 36px;
+        color: #fff;
+      `}
+    >
+      <img
+        css={css`
+          width: 160px;
+          height: 160px;
+        `}
+        src={KUchat_Logo}
+      ></img>
+      <div>KU chat</div>
+    </div>
+  )
+}
+
+export default Splash
