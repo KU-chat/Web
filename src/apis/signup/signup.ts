86f19c4369ef1854c21f79998dd836bdc58ff41f
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-01-20 17:45:46 +0900

    feat: signup api에 guestToken 추가

--- src/apis/signup/signup.ts
+++ src/apis/signup/signup.ts
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
 export const signup = async (signupData: any) => {
+  const guestToken = localStorage.getItem('guestToken')
+  if (!guestToken) {
+    throw new Error('Guest token이 존재하지 않습니다. 다시 시도해 주세요.')
+  }
   try {
-    const response = await axios.post('https://www.kuchat.site/member/signup', signupData)
+    const requestData = {
+      ...signupData,
+      guestToken,
+    }
+    const response = await axios.post('https://www.kuchat.site/member/signup', requestData)
     return response.data
   } catch (err: any) {
     throw new Error(err.message || '회원가입에 실패했습니다.')
