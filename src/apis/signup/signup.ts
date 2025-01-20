115ad58a8434c9b237004d946ef8c2ac50176f59
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-01-20 18:21:04 +0900

    fix: signup 요청 header 추가

--- src/apis/signup/signup.ts
+++ src/apis/signup/signup.ts
@@ -5,12 +5,18 @@ export const signup = async (signupData: any) => {
   if (!guestToken) {
     throw new Error('Guest token이 존재하지 않습니다. 다시 시도해 주세요.')
   }
+
   try {
-    const requestData = {
-      ...signupData,
-      guestToken,
-    }
-    const response = await axios.post('https://www.kuchat.site/member/signup', requestData)
+    const response = await axios.post(
+      'https://www.kuchat.site/member/signup',
+      signupData, // Body 데이터
+      {
+        headers: {
+          Authorization: `Bearer ${guestToken}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    )
     return response.data
   } catch (err: any) {
     throw new Error(err.message || '회원가입에 실패했습니다.')
