684f6e3f2335ca1829b88e39475ab1059923e77b
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-26 14:21:09 +0900

    build

--- src/apis/signup/signup.ts
+++ src/apis/signup/signup.ts
@@ -7,16 +7,12 @@ export const signup = async (signupData: any) => {
   }
 
   try {
-    const response = await axios.post(
-      'https://www.kuchat.site/member/signup',
-      signupData, // Body 데이터
-      {
-        headers: {
-          Authorization: `Bearer ${guestToken}`,
-          'Content-Type': 'application/json',
-        },
+    const response = await axios.post('https://www.kuchat.site/member/signup', signupData, {
+      headers: {
+        Authorization: `Bearer ${guestToken}`,
+        'Content-Type': 'application/json',
       },
-    )
+    })
     return response.data
   } catch (err: any) {
     throw new Error(err.message || '회원가입에 실패했습니다.')
