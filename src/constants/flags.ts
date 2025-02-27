6275ed6d9adc79f9da0f02cc1d5830c6953f9cbd
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-27 17:30:01 +0900

    feat: flagUrls

---
+++ src/constants/flags.ts
@@ -0,0 +1,10 @@
+export const flagImages: { [key: string]: string } = {
+  미국: 'https://flagcdn.com/us.svg',
+  일본: 'https://i.namu.wiki/i/uPDCkQv1zGpaEdmeqmEDRIM3nMyRD2BslQUouPpxpI5M-PkGdmxPwxFJvu9RCUUVYg2XOH4rfedfkxhnDqfumw.svg',
+  대한민국: 'https://flagcdn.com/kr.svg',
+  우즈베키스탄: 'https://flagcdn.com/uz.svg',
+  중국: 'https://flagcdn.com/cn.svg',
+  몽골: 'https://flagcdn.com/mn.svg',
+  프랑스: 'https://flagcdn.com/fr.svg',
+  베트남: 'https://flagcdn.com/vn.svg',
+}
